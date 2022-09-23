872224235c28f0a0c56f35394b7cbb72ac719a99
willow-rubenstein <contact.notashe@gmail.com>
2022-09-23 12:23:50 -0400

    Prototype is functional, work on ws server to communicate with js overlays

--- prototype.py
+++ prototype.py
@@ -1,45 +1,45 @@
-## Code based on https://github.com/Spikeedoo/SnooKey/blob/master/snookey.py
-import requests, json, os
+## Authentication Code based on https://github.com/Spikeedoo/SnooKey/blob/master/snookey.py
+
+import requests, json
 import time
 
-full_token = ""
 config = json.load(open("options.json"))
-cookie = ""
 reddit = requests.session()
 
 def get_v2_cookie():
+    isNotCookie = True
     global full_token
     global config
     global cookie
 
-    username = config['username']
-    password = config['password']
-    url = "https://ssl.reddit.com/api/login/" + username
+    while isNotCookie:
+        username = config['username']
+        password = config['password']
+        url = "https://ssl.reddit.com/api/login/" + username
 
-    payload = {
-        'api_type': 'json',
-        'user': username,
-        'passwd': password
-    }
+        payload = {
+            'api_type': 'json',
+            'user': username,
+            'passwd': password
+        }
 
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Project SnooKey/0.2'
-    }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Project SnooKey/0.2'
+        }
 
-    # Make login request
-    login_flow = reddit.request("POST", url, headers=headers, data=payload)
-    login_response = login_flow.json()
-    if len(login_response["json"]["errors"]) == 0:
-        # Login worked
-        # Capture session cookie
-        print(f"Got cookie, printing session cookie: {login_flow.cookies['reddit_session']}")
-        return login_flow.cookies["reddit_session"]
-    else:
-        print(login_response["json"]["errors"])
+        # Make login request
+        login_flow = reddit.request("POST", url, headers=headers, data=payload)
+        login_response = login_flow.json()
+        if len(login_response["json"]["errors"]) == 0:
+            print(f"Got cookie, printing session cookie: {login_flow.cookies['reddit_session']}")
+            isNotCookie = False
+            return login_flow.cookies["reddit_session"]
+        else:
+            print(f'Got errors: {login_response["json"]["errors"]}. retrying in 2s...')
+            time.sleep(2)
 
 def getFollowers():
-    global full_token
     cookie = get_v2_cookie()
     headers = {
             'Cookie': 'reddit_session=' + cookie,
@@ -49,10 +49,6 @@ def getFollowers():
     }
     while True:
         r = reddit.request("GET", url=f"https://www.reddit.com/user/h3llo_wor1d/about.json", headers=headers)
-        headers = {
-            'User-Agent': 'Project SnooKey/0.2',
-            'Authorization': "Bearer " + full_token
-        }
         json.dump(r.json(), open("reddit_out.json", "w+"), indent=4)
         time.sleep(1)
 
