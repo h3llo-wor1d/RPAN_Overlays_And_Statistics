349cb3dfa7891fb87237de61aac0a2b9e87058de
willow-rubenstein <contact.notashe@gmail.com>
2022-09-24 20:50:39 -0400

    finished, please fucking change the client id and client secret in bot.py to your own script app to make it work.

--- channelPointPrototype/proto.py
+++ channelPointPrototype/proto.py
@@ -6,10 +6,10 @@ clientID = "CnjCnliH6Az4NXLQ2LjuVg"
 
 def genAuth():
     reddit = praw.Reddit(
-        client_id="6WEeuqqUXAT9dlgnxIhbfg",
-        client_secret="4td21eC96le-FXn6uZKsHCvMwEe5BA",
+        client_id="",
+        client_secret="",
         redirect_uri="http://localhost:3000/redirect",
-        user_agent="Channel point-like system for rpan",
+        user_agent="",
     )
     print("Please give access to the following URL ")
     print(reddit.auth.url(scopes=["identity"], state="...", duration="permanent"))
@@ -18,14 +18,6 @@ def genAuth():
 
 def main():
     token = genAuth()
-    url = input("What is the url of the stream to listen to?").split("r/")[1].split("/")[1]
-    if os.path.exists(url) != True:
-        requests.post(f"https://strapi.reddit.com/videos/t3_{url}/heartbeat")
-        r = requests.get(urlPrefix+url, headers={"Authorization": f"Bearer {token}"})
-        print(r.json())
-    else:
-        ## Fetch websocket url from temp file
-        print("placeholder")
-    print(url)
+    
 
 main()
